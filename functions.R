6dae009906214aed238377cd22d41e83caa1a00a
Daniel Palamarchuk <danielpalamarchuk@gmail.com>
2022-03-01 14:53:32 -0500

    Uncommented shell function

--- functions.R
+++ functions.R
@@ -211,132 +211,132 @@ if (!show.legend) {
 return(gg)
 }
 
-#data_creator <- function(csv, color_id, split = FALSE) {
-#  a <- mutate(csv, Intro.Com = 1, Law = Passed)
-#  normal_a <- a %>% filter(is.na(Amended) | Amended == 0) %>%
-#    filter(is.na(Returned) | Returned == 0) 
-#  # Separate all the data with the initial return
-#  one_a <- a %>% filter(Amended == 1, Returned == 0)
-#  # Separate all the data with the second return
-#  two_a <- a %>% filter(Amended == 0, Returned == 1)
-#  # Separate all the data with both returns
-#  both_a <- a %>% filter(Amended == 1, Returned == 1)
-#  # Now to turn the data into something that can be read by the ggsankey
-#  normal_df <- normal_a %>% 
-#    make_long(Intro.Com,
-#              Pass.Com.1,
-#              Pass.Floor.1,
-#              Pass.Com.2,
-#              Pass.Floor.2,
-#              To.Gov,
-#              Passed,
-#              Law) %>% 
-#    na_if(0) %>% 
-#    filter(!is.na(node)) 
-#  
-#  one_df <- rbind(
-#    make_long(one_a,
-#              Intro.Com,
-#              Pass.Com.1,
-#              Pass.Floor.1,
-#              Pass.Com.2,
-#              Pass.Floor.2) %>% filter(!is.na(next_node)),
-#    make_long(one_a,
-#              Pass.Floor.2,
-#              Pass.Floor.1,
-#              To.Gov,
-#              Passed,
-#              Law)) %>% 
-#    na_if(0) %>% 
-#    filter(!is.na(node)) 
-#  two_df <- NULL
-#  both_df <- NULL
-# if (split) {
-#  two_df <- rbind(
-#    make_long(two_a,
-#              Intro.Com,
-#              Pass.Com.1,
-#              Pass.Floor.1,
-#              Pass.Com.2,
-#              Pass.Floor.2, 
-#              To.Gov) %>% filter(!is.na(next_node)),
-#    make_long(two_a,
-#              To.Gov,
-#              Pass.Floor.1,
-#              Passed),
-#    make_long(two_a,
-#              To.Gov,
-#              Pass.Floor.2,
-#              Passed,
-#              Law)) %>% 
-#    na_if(0) %>% 
-#    filter(!is.na(node)) 
-#  
-#  both_df <- rbind(
-#    make_long(both_a,
-#              Intro.Com,
-#              Pass.Com.1,
-#              Pass.Floor.1,
-#              Pass.Com.2,
-#              Pass.Floor.2) %>% filter(!is.na(next_node)),
-#    make_long(both_a,
-#              Pass.Floor.2,
-#              Pass.Floor.1,
-#              To.Gov) %>% filter(!is.na(next_node)),
-#    make_long(both_a,
-#              To.Gov,
-#              Pass.Floor.1,
-#              Passed) %>% filter(!is.na(next_node)),
-#    make_long(both_a,
-#              To.Gov,
-#              Pass.Floor.2,
-#              Passed,
-#              Law) %>% filter(!is.na(next_node))
-#  )
-# } else {
-#  two_df <- rbind(
-#    make_long(two_a,
-#              Intro.Com,
-#              Pass.Com.1,
-#              Pass.Floor.1,
-#              Pass.Com.2,
-#              Pass.Floor.2, 
-#              To.Gov) %>% filter(!is.na(next_node)),
-#    make_long(two_a,
-#              To.Gov,
-#              Pass.Floor.1,
-#              Pass.Floor.2,
-#              Passed,
-#              Law)
-#    ) %>% 
-#    na_if(0) %>% 
-#    filter(!is.na(node)) 
-#  
-#  both_df <- rbind(
-#    make_long(both_a,
-#              Intro.Com,
-#              Pass.Com.1,
-#              Pass.Floor.1,
-#              Pass.Com.2,
-#              Pass.Floor.2) %>% filter(!is.na(next_node)),
-#    make_long(both_a,
-#              Pass.Floor.2,
-#              Pass.Floor.1,
-#              To.Gov) %>% filter(!is.na(next_node)),
-#    make_long(both_a,
-#              To.Gov,
-#              Pass.Floor.1,
-#              Pass.Floor.2,
-#              Passed,
-#              Law) %>% filter(!is.na(next_node))
-#  )
-# 
-# }
-#  
-#  total_df <- rbind(normal_df, one_df, two_df)
-#  label <- levels(total_df$x)
-#  new_total_df <- total_df %>% select(x, next_x) %>% group_by(x, next_x) %>% summarize(n = n()) %>% 
-#    filter(!is.na(next_x)) %>% 
-#    mutate(color = color_id)
-#  return(new_total_df)
-#}
+data_creator_shell <- function(csv, color_id, split = FALSE) {
+  a <- mutate(csv, Intro.Com = 1, Law = Passed)
+  normal_a <- a %>% filter(is.na(Amended) | Amended == 0) %>%
+    filter(is.na(Returned) | Returned == 0) 
+  # Separate all the data with the initial return
+  one_a <- a %>% filter(Amended == 1, Returned == 0)
+  # Separate all the data with the second return
+  two_a <- a %>% filter(Amended == 0, Returned == 1)
+  # Separate all the data with both returns
+  both_a <- a %>% filter(Amended == 1, Returned == 1)
+  # Now to turn the data into something that can be read by the ggsankey
+  normal_df <- normal_a %>% 
+    make_long(Intro.Com,
+              Pass.Com.1,
+              Pass.Floor.1,
+              Pass.Com.2,
+              Pass.Floor.2,
+              To.Gov,
+              Passed,
+              Law) %>% 
+    na_if(0) %>% 
+    filter(!is.na(node)) 
+  
+  one_df <- rbind(
+    make_long(one_a,
+              Intro.Com,
+              Pass.Com.1,
+              Pass.Floor.1,
+              Pass.Com.2,
+              Pass.Floor.2) %>% filter(!is.na(next_node)),
+    make_long(one_a,
+              Pass.Floor.2,
+              Pass.Floor.1,
+              To.Gov,
+              Passed,
+              Law)) %>% 
+    na_if(0) %>% 
+    filter(!is.na(node)) 
+  two_df <- NULL
+  both_df <- NULL
+ if (split) {
+  two_df <- rbind(
+    make_long(two_a,
+              Intro.Com,
+              Pass.Com.1,
+              Pass.Floor.1,
+              Pass.Com.2,
+              Pass.Floor.2, 
+              To.Gov) %>% filter(!is.na(next_node)),
+    make_long(two_a,
+              To.Gov,
+              Pass.Floor.1,
+              Passed),
+    make_long(two_a,
+              To.Gov,
+              Pass.Floor.2,
+              Passed,
+              Law)) %>% 
+    na_if(0) %>% 
+    filter(!is.na(node)) 
+  
+  both_df <- rbind(
+    make_long(both_a,
+              Intro.Com,
+              Pass.Com.1,
+              Pass.Floor.1,
+              Pass.Com.2,
+              Pass.Floor.2) %>% filter(!is.na(next_node)),
+    make_long(both_a,
+              Pass.Floor.2,
+              Pass.Floor.1,
+              To.Gov) %>% filter(!is.na(next_node)),
+    make_long(both_a,
+              To.Gov,
+              Pass.Floor.1,
+              Passed) %>% filter(!is.na(next_node)),
+    make_long(both_a,
+              To.Gov,
+              Pass.Floor.2,
+              Passed,
+              Law) %>% filter(!is.na(next_node))
+  )
+ } else {
+  two_df <- rbind(
+    make_long(two_a,
+              Intro.Com,
+              Pass.Com.1,
+              Pass.Floor.1,
+              Pass.Com.2,
+              Pass.Floor.2, 
+              To.Gov) %>% filter(!is.na(next_node)),
+    make_long(two_a,
+              To.Gov,
+              Pass.Floor.1,
+              Pass.Floor.2,
+              Passed,
+              Law)
+    ) %>% 
+    na_if(0) %>% 
+    filter(!is.na(node)) 
+  
+  both_df <- rbind(
+    make_long(both_a,
+              Intro.Com,
+              Pass.Com.1,
+              Pass.Floor.1,
+              Pass.Com.2,
+              Pass.Floor.2) %>% filter(!is.na(next_node)),
+    make_long(both_a,
+              Pass.Floor.2,
+              Pass.Floor.1,
+              To.Gov) %>% filter(!is.na(next_node)),
+    make_long(both_a,
+              To.Gov,
+              Pass.Floor.1,
+              Pass.Floor.2,
+              Passed,
+              Law) %>% filter(!is.na(next_node))
+  )
+ 
+ }
+  
+  total_df <- rbind(normal_df, one_df, two_df)
+  label <- levels(total_df$x)
+  new_total_df <- total_df %>% select(x, next_x) %>% group_by(x, next_x) %>% summarize(n = n()) %>% 
+    filter(!is.na(next_x)) %>% 
+    mutate(color = color_id)
+  return(new_total_df)
+}
