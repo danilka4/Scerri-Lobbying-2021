94c45501c3b3b59215bb6949640aa744916b19e7
Daniel Palamarchuk <danielpalamarchuk@gmail.com>
2022-11-02 11:04:45 -0400

    Swing

---
+++ swing.R
@@ -0,0 +1,55 @@
+library(plotly)
+library(cowplot)
+source("functions.R")
+
+csv15 <- read.csv("data/csv_2015.csv") %>% col_care() %>% add_identifiers() %>% mutate(Year = 2015) %>%
+    separate(Com.1, into = c("Com.1", "Com.1.2", "Com.1.3", "Com.1.4"), sep = ";")
+csv16 <- read.csv("data/csv_2016.csv", nrows = 74) %>% col_care() %>% add_identifiers() %>% mutate(Year = 2016) %>%
+    separate(Com.1, into = c("Com.1", "Com.1.2", "Com.1.3", "Com.1.4"), sep = ";")
+csv17 <- read.csv("data/csv_2017.csv", nrows = 89) %>% col_care() %>% add_identifiers() %>% mutate(Year = 2017) %>%
+    separate(Com.1, into = c("Com.1", "Com.1.2", "Com.1.3", "Com.1.4"), sep = ";")
+csv18 <- read.csv("data/csv_2018.csv", nrows = 111) %>% col_care() %>% add_identifiers() %>% mutate(Year = 2018) %>%
+    separate(Com.1, into = c("Com.1", "Com.1.2", "Com.1.3", "Com.1.4"), sep = ";")
+csv19 <- read.csv("data/csv_2019.csv", nrows = 92) %>% col_care() %>% add_identifiers() %>% mutate(Year = 2019) %>%
+    separate(Com.1, into = c("Com.1", "Com.1.2", "Com.1.3", "Com.1.4"), sep = ";")
+csv20 <- read.csv("data/csv_2020.csv", nrows = 92) %>% col_care() %>% add_identifiers() %>% mutate(Year = 2020) %>%
+    separate(Com.1, into = c("Com.1", "Com.1.2", "Com.1.3", "Com.1.4"), sep = ";")
+csv21 <- read.csv("data/csv_2021.csv", nrows = 84) %>% col_care() %>% add_identifiers() %>% mutate(Year = 2021) %>%
+    separate(Com.1, into = c("Com.1", "Com.1.2", "Com.1.3", "Com.1.4"), sep = ";")
+
+csv_total <- rbind(csv15, csv16, csv17, csv18, csv19, csv20, csv21)
+
+politicians <- read.csv("data/politicians.csv")
+com_pol <- read.csv("data/committee_members.csv")
+swing <- read.csv("data/swing.csv") %>%
+    mutate(Swing = 1)
+
+comb <- left_join(com_pol, politicians, by = "Name") %>%
+    select(Year = Session, Committee, Name, District) %>%
+    left_join(swing, by = c("District", "Year")) %>%
+    mutate(Swing = coalesce(Swing, 0))
+rats <- filter(comb, Year %in% c(2017)) %>%
+    group_by(Year, Committee) %>%
+    summarize(SwingMembers = sum(Swing),
+    TotalMembers = n(),
+    SwingProp = mean(Swing))
+filter(rats, SwingProp == 1)
+bills <- filter(csv_total, Year %in% c(2017)) %>%
+    select(Bill, Year, Com.1, Pass.Com.1, Com.2, Pass.Com.2) %>%
+    group_by(Year, Com.1) %>%
+    summarize(TotalPassed = sum(Pass.Com.1), PropPassed = mean(Pass.Com.1))
+head(bills)
+completely_combined <- right_join(rats, bills, by = c("Year", "Committee" = "Com.1"))
+summary(completely_combined)
+completely_combined
+mod <- lm(PropPassed ~ SwingProp, completely_combined)
+summary(mod)
+ggplot(completely_combined, aes(SwingProp, PropPassed)) +
+    geom_point(aes(size = log(TotalPassed)), alpha = 0.5) +
+    geom_smooth(method=lm) +
+    theme_minimal() +
+    labs(size = "Committee Importance",
+    x = "Portion Committee in Swing District",
+    y = "Portion Bills Passed",
+    title = "Do Swing Districts Affect Bills Passing?")
+ggsave("swing_bill.png", bg = "white")
